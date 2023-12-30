6563f4cdb5e613f0cff56079aafad1d2441f1332
dxdns <dxdns@hotmail.com>
2023-12-30 18:02:50 -0300

    button component ref

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,16 @@
+import React from "react"
 import "./style.css"
 
-type Props = {
-    onClick?: () => void
-    children: JSX.Element
-}
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & React.PropsWithChildren<{}>
 
-export default function Button({ onClick, children }: Props) {
+export default React.forwardRef<HTMLButtonElement, Props>(({ children, ...rest }, ref) => {
     return (
-        <button className="button" onClick={onClick}>
+        <button
+            ref={ref}
+            {...rest}
+            className="button"
+        >
             {children}
         </button>
     )
-}
+})
