b2f752fce3e6bc5361de9534654bc423340cbf0c
dxdns <dxdns@hotmail.com>
2024-01-05 09:34:16 -0300

    pagination component handler

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -2,28 +2,48 @@ import Icon from "../icon"
 import "./style.css"
 
 type Props = {
+    rowsPerPage: number
     count: number
+    currentPage: number
+    onPageChange: (newPage: number) => void
 }
 
-export default function Pagination({ count }: Props) {
+export default function Pagination({ rowsPerPage, count, currentPage, onPageChange }: Props) {
+    const nPage = Math.ceil(count / rowsPerPage)
+    const numbers = [...Array(nPage + 1).keys()].slice(1)
+
+    function prePage() {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1)
+        }
+    }
+
+    function nextPage() {
+        if (currentPage < nPage) {
+            onPageChange(currentPage + 1)
+        }
+    }
+
     return (
         <ul className="pagination">
-            <li>
+            <li onClick={prePage}>
                 <Icon
                     name="chevron_left"
                     variant="text"
                 />
             </li>
 
-            {[...Array(count)].map((_, index) => (
-                <div key={index}>
-                    <li className={`${index + 1 === 4 ? "active" : ""}`}>
-                        {index + 1}
-                    </li>
-                    {/* <li className="dots">...</li> */}
-                </div>))}
+            {numbers.map((n, i) => (
+                <li
+                    key={i}
+                    className={`${n === currentPage ? "active" : ""}`}
+                    onClick={() => onPageChange(n)}
+                >
+                    {n}
+                </li>
+            ))}
 
-            <li>
+            <li onClick={nextPage}>
                 <Icon
                     name="chevron_right"
                     variant="text"
--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -1,8 +1,18 @@
+import React from "react"
 import Badge from "../../components/badge"
 import Button from "../../components/button"
 import Pagination from "../../components/pagination"
 
 export default function Customer() {
+    const [currentPage, setCurrentPage] = React.useState(1)
+
+    const data = Array.from({ length: 50 }, (_, i) => ({ id: i, name: `test${i}` }))
+
+    const rowsPerPage = 10
+    const lastIndex = currentPage * rowsPerPage
+    const firstIndex = lastIndex - rowsPerPage
+    const result = data.slice(firstIndex, lastIndex)
+
     return (
         <>
             <h1>Page Customer</h1>
@@ -12,7 +22,17 @@ export default function Customer() {
             <Badge variant="outlined">
                 Opa
             </Badge>
-            <Pagination count={10} />
+            <div style={{ padding: "5rem" }}>
+                {result.map((item) => (
+                    <strong key={item.id}>{item.name}</strong>
+                ))}
+            </div>
+            <Pagination
+                count={data.length}
+                rowsPerPage={rowsPerPage}
+                currentPage={currentPage}
+                onPageChange={(newPage) => setCurrentPage(newPage)}
+            />
         </>
     )
 }
