a06c7a1fda8d967223055d859315ee96212a229e
dxdns <dxdns@hotmail.com>
2024-01-15 17:49:03 -0300

    pin input: mask | handle value

--- src/components/pin-input/index.tsx
+++ src/components/pin-input/index.tsx
@@ -1,12 +1,30 @@
 import "./style.css"
 
-export default function PinInput() {
+type Props = {
+    mask?: boolean
+}
+
+export default function PinInput({ mask }: Props) {
+    function handleInput({ currentTarget }: React.FormEvent<HTMLInputElement>) {
+        const value = currentTarget.value
+        const max = currentTarget.maxLength
+
+        currentTarget.value = value.length > max ? value.slice(0, max) : value
+    }
+
     return (
         <div className="pin-input">
-            <input type="text" placeholder="○" maxLength={1} autoFocus />
-            <input type="text" placeholder="○" maxLength={1} />
-            <input type="text" placeholder="○" maxLength={1} />
-            <input type="text" placeholder="○" maxLength={1} />
+            {[...Array(4)].map((_, index) => (
+                <input
+                    key={index}
+                    type={!mask ? "number" : "password"}
+                    inputMode="numeric"
+                    placeholder="○"
+                    maxLength={1}
+                    onInput={handleInput}
+                />
+            ))}
+
         </div>
     )
 }
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import Button from "../../components/button"
 import Input from "../../components/input"
 import "./style.css"
+import PinInput from "../../components/pin-input"
 
 export default function SignIn() {
     return (
@@ -17,6 +18,7 @@ export default function SignIn() {
             <div className="content">
                 <Input type="email" label="Email" required />
                 <Input type="password" label="Password" required />
+                <PinInput />
                 <Link to={"forgot-password"}>Forgot password?</Link>
                 <Button type="submit">Sign In</Button>
             </div>
