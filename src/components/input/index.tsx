0113e345d103fcd2644db0156a064a95e45b0a7b
dxdns <dxdns@hotmail.com>
2024-01-16 09:13:16 -0300

    handle input password

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,21 +1,37 @@
 import React from "react"
 import "./style.css"
+import IconButton from "../icon-button"
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
     label?: string
 }
 
 export default React.forwardRef<HTMLInputElement, Props>(({ label, ...rest }, ref) => {
+    const [passwordVisible, setPasswordVisible] = React.useState(false)
+    const inputRef = React.useRef<HTMLInputElement | null>(null)
+
+    function handleInputPassword() {
+        setPasswordVisible(old => !old)
+        inputRef.current?.focus()
+    }
+
     return (
         <div className={`input-group ${rest.required ? "required" : ""}`}>
             <input
-                ref={ref}
+                ref={ref || inputRef}
                 {...rest}
+                type={passwordVisible ? "text" : rest.type}
                 autoComplete="off"
             />
             {label && <label htmlFor="">
                 {label}
             </label>}
+            {rest.type === "password" &&
+                <IconButton
+                    type="button"
+                    name={passwordVisible ? "visibility_off" : "visibility"}
+                    onClick={handleInputPassword}
+                />}
         </div>
     )
 })
