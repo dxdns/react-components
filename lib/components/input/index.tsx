11743976271afdec1c455e09360bcb7bd36a6965
dxdns <37380674+dxdns@users.noreply.github.com>
2025-02-06 10:17:44 -0300

    remove input handle

--- lib/components/input/index.tsx
+++ lib/components/input/index.tsx
@@ -15,57 +15,6 @@ export default React.forwardRef<HTMLInputElement, Props>(
         const [passwordVisible, setPasswordVisible] = React.useState(false)
         const inputRef = React.useRef<HTMLInputElement | null>(null)
 
-        function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-            if (rest.type === "number") {
-                let newValue = event.target.value
-
-                inputRef.current!.value = newValue
-
-                const numericValue = parseFloat(newValue)
-                if (!Number.isNaN(numericValue)) {
-                    newValue = numericValue.toString()
-                }
-
-                const min = rest.min ? Number(rest.min) : 0
-                if (min || Number(min) === 0) {
-                    if (numericValue <= min) {
-                        newValue = min.toString()
-                    }
-                }
-
-                const max = rest.max ? Number(rest.max) : Infinity
-                if (max) {
-                    if (numericValue >= max) {
-                        newValue = max.toString()
-                    }
-                }
-            }
-
-            rest.onChange && rest.onChange(event)
-        }
-
-        function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-            const value = inputRef.current!.value
-            const hasDecimalPoint = value.includes(".")
-
-            if (event.key === "." && hasDecimalPoint) {
-                event.preventDefault()
-            }
-        }
-
-        function handleBlur(
-            event: React.FocusEvent<HTMLInputElement, Element>,
-        ) {
-            if (rest.type === "number") {
-                const numericValue = parseFloat(inputRef.current!.value)
-
-                if (!isNaN(numericValue)) {
-                    inputRef.current!.value = numericValue.toFixed(2)
-                }
-            }
-            rest.onBlur && rest.onBlur(event)
-        }
-
         function handleInputPassword() {
             setPasswordVisible((old) => !old)
             inputRef.current?.focus()
@@ -89,9 +38,6 @@ export default React.forwardRef<HTMLInputElement, Props>(
                     placeholder={
                         rest.type === "search" ? rest.placeholder : " "
                     }
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    onKeyDown={handleKeyDown}
                 />
                 {label && <label htmlFor={label}>{label}</label>}
                 {rest.type === "password" && (
