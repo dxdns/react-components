90eaa8ec51e8c89f71d1893e67eda84d3ea08f89
dxdns <37380674+dxdns@users.noreply.github.com>
2025-02-06 09:37:51 -0300

    handle input

--- lib/components/input/index.tsx
+++ lib/components/input/index.tsx
@@ -1,9 +1,9 @@
 import styles from "./styles.module.css"
 import React from "react"
 import IconButton from "../icon-button"
-import { VariantType } from "../../types"
 import { FaSearch } from "react-icons/fa"
 import { MdVisibility, MdVisibilityOff } from "react-icons/md"
+import { VariantType } from "lib/types"
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
     label?: string
@@ -15,32 +15,55 @@ export default React.forwardRef<HTMLInputElement, Props>(
         const [passwordVisible, setPasswordVisible] = React.useState(false)
         const inputRef = React.useRef<HTMLInputElement | null>(null)
 
-        function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
             if (rest.type === "number") {
-                let newValue = parseFloat(e.target.value)
+                let newValue = event.target.value
 
-                if (Number.isNaN(newValue)) {
-                    console.error("invalid")
+                inputRef.current!.value = newValue
+
+                const numericValue = parseFloat(newValue)
+                if (!Number.isNaN(numericValue)) {
+                    newValue = numericValue.toString()
                 }
 
-                if (rest.min || Number(rest.min) === 0) {
-                    let min = Number(rest.min || 0)
-                    if (newValue <= min) {
-                        newValue = min
+                const min = rest.min ? Number(rest.min) : 0
+                if (min || Number(min) === 0) {
+                    if (numericValue <= min) {
+                        newValue = min.toString()
                     }
                 }
 
-                if (rest.max) {
-                    let max = Number(rest.max || 0)
-                    if (newValue >= max) {
-                        newValue = max
+                const max = rest.max ? Number(rest.max) : Infinity
+                if (max) {
+                    if (numericValue >= max) {
+                        newValue = max.toString()
                     }
                 }
-
-                inputRef.current!.value = String(newValue)
             }
 
-            rest.onChange && rest.onChange(e)
+            rest.onChange && rest.onChange(event)
+        }
+
+        function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+            const value = inputRef.current!.value
+            const hasDecimalPoint = value.includes(".")
+
+            if (event.key === "." && hasDecimalPoint) {
+                event.preventDefault()
+            }
+        }
+
+        function handleBlur(
+            event: React.FocusEvent<HTMLInputElement, Element>,
+        ) {
+            if (rest.type === "number") {
+                const numericValue = parseFloat(inputRef.current!.value)
+
+                if (!isNaN(numericValue)) {
+                    inputRef.current!.value = numericValue.toFixed(2)
+                }
+            }
+            rest.onBlur && rest.onBlur(event)
         }
 
         function handleInputPassword() {
@@ -67,6 +90,8 @@ export default React.forwardRef<HTMLInputElement, Props>(
                         rest.type === "search" ? rest.placeholder : " "
                     }
                     onChange={handleChange}
+                    onBlur={handleBlur}
+                    onKeyDown={handleKeyDown}
                 />
                 {label && <label htmlFor={label}>{label}</label>}
                 {rest.type === "password" && (
